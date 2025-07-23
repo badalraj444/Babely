d00d23423df15fa55f6ccd7214e9fb10caebc528
badalraj444 <badalraj444@gmail.com>
2025-07-23 14:11:10 +0530

    deployment ready

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,16 +6,18 @@ import {connectDB} from "./lib/db.js"
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import chatRoutes from "./routes/chat.route.js";
-
+import path from "path";
 
 const app = express();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT; //
 
 app.use(cors({
     origin: "http://localhost:5173",
     credentials: true
 }));
+
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
@@ -23,6 +25,14 @@ app.use(cookieParser());
 app.use("/api/auth",authRoutes);
 app.use("/api/users",userRoutes);
 app.use("/api/chat", chatRoutes);
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"../client","dist")));
+    app.get("*",(req,res)=>{
+        res.sendFile(path.join(__dirname,"../client","dist","index.html"));
+    });
+}
+
 app.listen(PORT,()=>{
     console.log(`server listening on port ${PORT}`);
     connectDB();
